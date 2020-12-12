0204aef6603347ffd31125eb32df228cd149e9dd
Alfiya Mokashi <alfiyamokashi72@gmail.com>
2020-12-12 12:32:44 +0530

    removed all added lines earlier

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,6 @@
 public class HelloWorld{
     public static void main(String args[]) {
         System.out.println("Hello World from app");
-        System.out.println("Added another line");
-	  System.out.println("Added another line");
-  System.out.println("Added another line");
-  System.out.println("Added another line");
-  System.out.println("Added another line");
-  System.out.println("Added another line");
-int num1=30;
-float salary=909800;
 System.out.println("Alfiya Here");
 
     }
